813d4d41a45fccdb30dc862b24c6bdbadf7a0ad0
handawei <david@weimilai.com>
2018-09-08 18:33:53 +0800

    去

--- src/test/LIUJIE.java
+++ src/test/LIUJIE.java
@@ -4,7 +4,8 @@ public class LIUJIE {
   public static void main(String[] args) {
 	System.out.println("liujie");
 	System.out.println("liujie1");
-	System.out.println("liujie10");
-
+	System.out.println("liujie5");
+	System.out.println("liujie5");
+	System.out.println("liujie5");
 }
 }
