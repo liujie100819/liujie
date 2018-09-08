495d58129caf48b0361cefba11befe3d823130ec
handawei <david@weimilai.com>
2018-09-08 18:30:29 +0800

    订单

--- src/test/LIUJIE.java
+++ src/test/LIUJIE.java
@@ -3,6 +3,7 @@ package test;
 public class LIUJIE {
   public static void main(String[] args) {
 	System.out.println("liujie");
-	System.out.println("liujie1");
+	System.out.println("liujie2");
+
 }
 }
